efc067dd99eb89d4f3c685acfe45c49d094cb0e5
Efe Şengül <86547599+Efesngl@users.noreply.github.com>
2024-05-03 16:15:01 +0300

    credential system updated

--- server.js
+++ server.js
@@ -4,48 +4,104 @@ import fs from "fs";
 import QueryString from "qs";
 
 const app = e();
-let credentials=null
+let credentials = null;
 
 async function setCredentials() {
-  let c=await getCredentials()
-  credentials.userSign=c.us
-  credentials.webID=c.wi
-  credentials.timestamp=c.ts
-  fs.writeFileSync("./creds.json",JSON.stringify(credentials),"utf8")
-  return true
+  let c = await getCredentials();
+  credentials.userSign = c.us;
+  credentials.webID = c.wi;
+  credentials.timestamp = c.ts;
+  fs.writeFileSync("./creds.json", JSON.stringify(credentials), "utf8");
+  return true;
 }
 
 app.get("/getads", async (req, res) => {
   let qs = QueryString.stringify(req.query);
-  let ads = await getAds(credentials.userSign, credentials.webID, credentials.timestamp, qs);
+  let ads = await getAds(
+    credentials.userSign,
+    credentials.webID,
+    credentials.timestamp,
+    qs
+  );
   if (ads.code != 0) {
     console.log("get ads cred");
     await setCredentials();
-    ads = await getAds(credentials.userSign, credentials.webID, credentials.timestamp, qs);
+    ads = await getAds(
+      credentials.userSign,
+      credentials.webID,
+      credentials.timestamp,
+      qs
+    );
   }
   res.json(ads);
 });
 app.get("/getadsdetail", async (req, res) => {
   let qs = QueryString.stringify(req.query);
-  let ads = await getAds(credentials.userSign, credentials.webID, credentials.timestamp, qs);
+  let qsp = QueryString.parse(req.query);
+  let ads = await getAds(
+    credentials.userSign,
+    credentials.webID,
+    credentials.timestamp,
+    qs
+  );
   if (ads.code != 0) {
     console.log("get adsd etails cred");
     await setCredentials();
-    ads = await getAds(credentials.userSign, credentials.webID, credentials.timestamp, qs);
+    ads = await getAds(
+      credentials.userSign,
+      credentials.webID,
+      credentials.timestamp,
+      qs
+    );
   }
-  let adsArray = [];
+  let adsArray = {};
   for (const ad of ads.data.materials) {
-    let data = await delayedFetch(ad.id, credentials.userSign, credentials.webID, credentials.timestamp, 500);
-    adsArray.push(data.data);
+    let data = await delayedFetch(
+      ad.id,
+      credentials.userSign,
+      credentials.webID,
+      credentials.timestamp,
+      500
+    );
+    adsArray[ad.id] = data.data;
   }
+  let date = new Date();
+  adsArray["filters"] = {
+    period: qsp.period,
+    industry: qsp.industry,
+    order_by: qsp.order_by,
+    ad_language: qsp.ad_language,
+    country: qsp.country,
+    like: qsp.like,
+    objective: qsp.objective,
+  };
+  fs.writeFileSync(
+    `ads_${date.getDay()}-${date.getMonth()}-${date.getFullYear()}/period_${
+      qsp["period"]
+    }.json`,
+    JSON.stringify(adsArray),
+    "utf8"
+  );
   res.json(adsArray);
 });
 app.get("/getaddetail/:adid", async (req, res) => {
-  let detail = await delayedFetch(req.params.adid, credentials.userSign, credentials.webID, credentials.timestamp, 500);
+  let detail = await delayedFetch(
+    req.params.adid,
+    credentials.userSign,
+    credentials.webID,
+    credentials.timestamp,
+    500
+  );
   if (detail.code != 0) {
     console.log("get ad detail cred");
     await setCredentials();
-    detail = await delayedFetch(req.params.adid, credentials.userSign, credentials.webID, credentials.timestamp, 500);
+    detail = await delayedFetch(
+      req.params.adid,
+      credentials.userSign,
+      credentials.webID,
+      credentials.timestamp,
+      500
+    );
   }
   res.json(detail);
 });
@@ -53,14 +109,35 @@ app.get("/filters", (req, res) => {
   var file = fs.readFileSync("./filters.json", "utf8");
   res.json(JSON.parse(file));
 });
+app.get("/getfile/", async (req, res) => {
+  let qs = QueryString.parse(req.query);
+  let file = fs.readFileSync(qs.f, { encoding: "utf8" });
+  res.json(JSON.parse(file));
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, async () => {
-  credentials=await JSON.parse(fs.readFileSync("./creds.json",{encoding:"utf8"}))
-  if(credentials.userSign==""||credentials.webID==""||credentials.timestamp==""){
+  if (!fs.existsSync("./creds.json")) {
+    let data = {
+      userSign: "",
+      webID: "",
+      timestamp: "",
+    };
+    fs.writeFileSync("./creds.json", JSON.stringify(data), {
+      encoding: "utf8",
+    });
+  }
+  credentials = await JSON.parse(
+    fs.readFileSync("./creds.json", { encoding: "utf8" })
+  );
+  if (
+    credentials.userSign == "" ||
+    credentials.webID == "" ||
+    credentials.timestamp == ""
+  ) {
     console.log("Setting credentials please wait !!!");
-    await setCredentials()
-    console.clear()
+    await setCredentials();
+    console.clear();
     console.log("Done !");
   }
   console.log("app listens at " + port);
